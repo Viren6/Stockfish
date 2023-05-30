5a19dcfc290e936ee289c77b7412ee50a3260e63
Viren6 <94880762+Viren6@users.noreply.github.com>
2023-05-30 23:44:53 +0100

    Increase Condition
    Increase reduction if our move count is high

--- src/search.cpp
+++ src/search.cpp
@@ -1143,6 +1143,10 @@ moves_loop: // When in check, search starts here
       if ((ss-1)->moveCount > 8)
           r--;
 
+      // Increase reduction if our move count is high
+      if ((ss)->moveCount > 8)
+          r++;
+
       // Increase reduction for cut nodes (~3 Elo)
       if (cutNode)
           r += 2;
