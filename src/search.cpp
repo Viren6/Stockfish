616d77003913307868c32f97fac508d8d3dff616
Viren6 <94880762+Viren6@users.noreply.github.com>
2023-06-27 20:39:42 +0100

    bench 2570880
    Take 7

--- src/search.cpp
+++ src/search.cpp
@@ -1171,7 +1171,7 @@ moves_loop: // When in check, search starts here
           r--;
 
       // Increase reduction based on PSQ
-      r += std::max(0, int(pos.psq_eg_stm()) / 2048);
+      r += std::max(0, int(pos.psq_eg_stm()) / 1024);
 
       ss->statScore =  2 * thisThread->mainHistory[us][from_to(move)]
                      + (*contHist[0])[movedPiece][to_sq(move)]
