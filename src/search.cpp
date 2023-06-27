c0017fbfc7c4d7edaf7d90b5323d9ac00a48ca30
Viren6 <94880762+Viren6@users.noreply.github.com>
2023-06-27 15:29:00 +0100

    bench 2945184
    Take 1

--- src/search.cpp
+++ src/search.cpp
@@ -1167,6 +1167,10 @@ moves_loop: // When in check, search starts here
       if ((ss+1)->cutoffCnt > 3)
           r++;
 
+      // Increase reduction if PSQ advantage is decisive
+      if (pos.psq_eg_stm() > 2048)
+          r++;
+
       else if (move == ttMove)
           r--;
 
