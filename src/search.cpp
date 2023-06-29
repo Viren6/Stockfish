fc6548b2127c3f25faf3ab9facf38cacce9190a7
Viren6 <94880762+Viren6@users.noreply.github.com>
2023-06-29 12:30:52 +0100

    bench 2473482
    Take 2

--- src/search.cpp
+++ src/search.cpp
@@ -1171,7 +1171,7 @@ moves_loop: // When in check, search starts here
           r--;
 
       if (eval < alpha - 456 - 252 * depth * depth)
-          r++;
+          r--;
 
       ss->statScore =  2 * thisThread->mainHistory[us][from_to(move)]
                      + (*contHist[0])[movedPiece][to_sq(move)]
