1c465f0e2b5a57d472f41d157bbe744d9900c702
Viren6 <94880762+Viren6@users.noreply.github.com>
2023-06-29 13:34:29 +0100

    bench 2426246
    Take 5

--- src/search.cpp
+++ src/search.cpp
@@ -1173,7 +1173,7 @@ moves_loop: // When in check, search starts here
           r--;
 
       if (razor)
-          r += 2;
+          r += 3;
 
       ss->statScore =  2 * thisThread->mainHistory[us][from_to(move)]
                      + (*contHist[0])[movedPiece][to_sq(move)]
