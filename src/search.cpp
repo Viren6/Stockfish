58c4243a305218fe363c4a04b7c301cf706e13cb
Viren6 <94880762+Viren6@users.noreply.github.com>
2023-07-02 14:24:17 +0100

    bench 2521048
    Take 6

--- src/search.cpp
+++ src/search.cpp
@@ -1148,7 +1148,7 @@ moves_loop: // When in check, search starts here
           r -= cutNode && tte->depth() >= depth + 3 ? 3 : 2;
 
       // Decrease reduction if opponent's move count is high (~1 Elo)
-      r -= (ss-1)->moveCount / 8;
+      r -= int(sqrt((ss-1)->moveCount)) / 3;
 
       // Increase reduction for cut nodes (~3 Elo)
       if (cutNode)
