2a52892408015af67e1923abf28c37b1689cb138
Viren6 <94880762+Viren6@users.noreply.github.com>
2023-07-02 18:21:07 +0100

    bench 2458747
    Take 1

--- src/search.cpp
+++ src/search.cpp
@@ -1147,10 +1147,6 @@ moves_loop: // When in check, search starts here
           && !likelyFailLow)
           r -= cutNode && tte->depth() >= depth + 3 ? 3 : 2;
 
-      // Decrease reduction if opponent's move count is high (~1 Elo)
-      if ((ss-1)->moveCount >= 9)
-          r--;
-
       // Increase reduction for cut nodes (~3 Elo)
       if (cutNode)
           r += 2;
@@ -1167,12 +1163,7 @@ moves_loop: // When in check, search starts here
       if (singularQuietLMR)
           r--;
 
-      // Increase reduction if next ply has a lot of fail high (~5 Elo)
-      if ((ss+1)->cutoffCnt >= 4)
-          r++;
-
-      if ((ss+1)->cutoffCnt >= 16)
-          r++;
+      r += std::clamp(((ss+1)->cutoffCnt * 4 - (ss-1)->moveCount) / 8, -1, 1);
 
       if (move == ttMove && (ss+1)->cutoffCnt < 4)
           r--;
