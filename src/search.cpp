c68a870fc1e42f4acfb136ac92f0b158086e3ca3
Viren6 <94880762+Viren6@users.noreply.github.com>
2023-07-03 20:42:15 +0100

    bench 2599126
    Take 1

--- src/search.cpp
+++ src/search.cpp
@@ -1163,11 +1163,7 @@ moves_loop: // When in check, search starts here
       if (singularQuietLMR)
           r--;
 
-      int adjustR = std::clamp(((ss+1)->cutoffCnt * 370 - (ss-1)->moveCount * 100) / 762, -1, 1);
-      r += adjustR;
-
-      if (move == ttMove && adjustR != 1)
-          r--;
+      r += std::clamp(((ss+1)->cutoffCnt * 370 - (ss-1)->moveCount * 100 - move==ttMove * 729) / 729, -1, 1);
 
       ss->statScore =  2 * thisThread->mainHistory[us][from_to(move)]
                      + (*contHist[0])[movedPiece][to_sq(move)]
