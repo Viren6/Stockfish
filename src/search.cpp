9332cd1a9044b4286dbd9662bb7453f71aef84c7
Viren6 <94880762+Viren6@users.noreply.github.com>
2023-07-04 02:35:29 +0100

    bench 2528289
    Simplify: Decrease reduction if ttMove has been singularly extended

--- src/search.cpp
+++ src/search.cpp
@@ -1164,10 +1164,6 @@ moves_loop: // When in check, search starts here
       if (PvNode)
           r -= 1 + 12 / (3 + depth);
 
-      // Decrease reduction if ttMove has been singularly extended (~1 Elo)
-      if (singularQuietLMR)
-          r--;
-
       // Increase reduction if next ply has a lot of fail high (~5 Elo)
       if ((ss+1)->cutoffCnt > 3)
           r++;
