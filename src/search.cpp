2dc91b64487ae3a8f3180c74e27661e8662b4721
Viren6 <94880762+Viren6@users.noreply.github.com>
2023-07-21 08:57:33 +0100

    Cleanup

--- src/search.cpp
+++ src/search.cpp
@@ -119,33 +119,22 @@ namespace {
   int Extensions[2][2][2][2][2][2][2][2][2][2][2][2];
 
   void extensionsCacher() {
-      for (int i = 0; i < 2; ++i) {
-          for (int j = 0; j < 2; ++j) {
-              for (int k = 0; k < 2; ++k) {
-                  for (int l = 0; l < 2; ++l) {
-                      for (int m = 0; m < 2; ++m) {
-                          for (int n = 0; n < 2; ++n) {
-                              for (int o = 0; o < 2; ++o) {
-                                  for (int p = 0; p < 2; ++p) {
-                                      for (int q = 0; q < 2; ++q) {
-                                          for (int r = 0; r < 2; ++r) {
-                                              for (int s = 0; s < 2; ++s) {
+      for (int i = 0; i < 2; ++i)
+          for (int j = 0; j < 2; ++j)
+              for (int k = 0; k < 2; ++k) 
+                  for (int l = 0; l < 2; ++l)
+                      for (int m = 0; m < 2; ++m)
+                          for (int n = 0; n < 2; ++n)
+                              for (int o = 0; o < 2; ++o)
+                                  for (int p = 0; p < 2; ++p)
+                                      for (int q = 0; q < 2; ++q)
+                                          for (int r = 0; r < 2; ++r)
+                                              for (int s = 0; s < 2; ++s)
                                                   for (int t = 0; t < 2; ++t) {
                                                       bool W_IN[12] = { i,j,k,l,m,n,o,p,q,r,s,t };
                                                       Extensions[i][j][k][l][m][n][o][p][q][r][s][t] =
                                                           calculateExtension(W_IN);
                                                   }
-                                              }
-                                          }
-                                      }
-                                  }
-                              }
-                          }
-                      }
-                  }
-              }
-          }
-      }
   }
 
   int extensionsLookup(bool W_IN[12]) {
