9f704d1aadeddd5bab35aed28eee2dd4c0ad4ae6
Viren6 <94880762+Viren6@users.noreply.github.com>
2023-07-28 01:55:12 +0100

    pr check

--- src/search.cpp
+++ src/search.cpp
@@ -173,9 +173,9 @@ namespace {
   int outputBias[2] = { -34, 161 };
   int outputSlopes[2][2] = { { 1007, 779 }, {951, 806} };
 
-  TUNE(SetRange(-4096, 4096), inputScales, depthInput, singularInput, statScoreInput, SetRange(-16384, 16384), biases, outputBias, SetRange(0, 8192), slopes, outputSlopes);
+  //TUNE(SetRange(-4096, 4096), inputScales, depthInput, singularInput, statScoreInput, SetRange(-16384, 16384), biases, outputBias, SetRange(0, 8192), slopes, outputSlopes);
 
-  TUNE( SetRange(400, 2000), lmrDepthScale, lmrDepthScaleTwo, SetRange(800, 4000), ttMoveCutNodeScale, 
+  //TUNE( SetRange(400, 2000), lmrDepthScale, lmrDepthScaleTwo, SetRange(800, 4000), ttMoveCutNodeScale, 
       SetRange(1600, 10000), depthReductionDecreaseThres, SetRange(-12000, -1500), LMRDepthReductionThres);
 
   int PReLU(int input, int negativeSlope, int positiveSlope) {
