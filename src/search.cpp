6fdc9a56d1c890a7084a9af719f66b2595c95ae1
Viren6 <94880762+Viren6@users.noreply.github.com>
2023-08-30 23:41:26 +0100

    Take 3
    bench 1503774

--- src/search.cpp
+++ src/search.cpp
@@ -1213,8 +1213,8 @@ moves_loop: // When in check, search starts here
           if (!ttMove && !PvNode)
               r += 1 + cutNode;
 
-          if ((ss+1)->cutoffCnt > 3 && !ss->ttPv)
-              r += 1 + cutNode;
+          if ((ss+1)->cutoffCnt > 3 && !ss->ttPv && !(!ttMove && cutNode))
+              r++;
 
           value = -search<NonPV>(pos, ss+1, -(alpha+1), -alpha, newDepth - (r > 3), !cutNode);
       }
