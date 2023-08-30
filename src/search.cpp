b0adba4ac1ffdb02641e5fc72f525222f50ef97f
Viren6 <94880762+Viren6@users.noreply.github.com>
2023-08-30 16:09:34 +0100

    Take 4
    bench 1575147

--- src/search.cpp
+++ src/search.cpp
@@ -1213,8 +1213,8 @@ moves_loop: // When in check, search starts here
           if (!ttMove && cutNode)
               r += 2;
 
-          if ((ss+1)->cutoffCnt > 3 && !ss->ttPv)
-              r++;
+          if (singularQuietLMR && !ttCapture)
+              r--;
 
           value = -search<NonPV>(pos, ss+1, -(alpha+1), -alpha, newDepth - (r > 3), !cutNode);
       }
