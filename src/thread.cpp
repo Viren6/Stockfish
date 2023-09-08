d9421726a5ca357d6211e5bc25e26ddb20b69c7d
Viren6 <94880762+Viren6@users.noreply.github.com>
2023-09-08 23:16:26 +0100

    Take 1
    bench 1599294

--- src/thread.cpp
+++ src/thread.cpp
@@ -213,7 +213,7 @@ void ThreadPool::start_thinking(Position& pos, StateListPtr& states,
       th->rootMoves = rootMoves;
       th->rootPos.set(pos.fen(), pos.is_chess960(), &th->rootState, th);
       th->rootState = setupStates->back();
-      th->rootSimpleEval = Eval::simple_eval(pos, pos.side_to_move());
+      th->rootSimpleEval = Value(Eval::simple_eval(pos, pos.side_to_move(), pos.rule50_count()));
   }
 
   main()->start_searching();
