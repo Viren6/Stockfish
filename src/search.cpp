839826955861cd20075ebf7d0846473d6313d95f
Viren6 <94880762+Viren6@users.noreply.github.com>
2023-09-11 03:31:16 +0100

    Take 5
    bench 1397193

--- src/search.cpp
+++ src/search.cpp
@@ -736,9 +736,11 @@ namespace {
 
         // ttValue can be used as a better position evaluation (~7 Elo)
         if (    ttValue != VALUE_NONE
-            && (tte->bound() & (ttValue > eval ? BOUND_LOWER : BOUND_UPPER))
-            && (tte->depth() > 0))
-            eval = (eval + ttValue * tte->depth()) / (1 + tte->depth());
+            && (tte->bound() & (ttValue > eval ? BOUND_LOWER : BOUND_UPPER)))
+        {
+            int weighting = tte->depth() + 1;
+            eval = (eval + ttValue * weighting) / (1 + weighting);
+        }
     }
     else
     {
