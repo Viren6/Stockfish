c424dc5aa36b7e9fb31963191eb8d1290d05889e
Viren6 <94880762+Viren6@users.noreply.github.com>
2023-09-14 08:01:16 +0100

    Take 3
    bench 1392592

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -176,7 +176,7 @@ Value Eval::evaluate(const Position& pos) {
       nnue     -= nnue     * (nnueComplexity + abs(simpleEval - nnue)) / 32768;
 
       int npm = pos.non_pawn_material() / 64;
-      v = (  nnue     * (915 + npm + 9 * pos.count<PAWN>() - 5 * shuffling)
+      v = (  nnue     * (915 + npm + 9 * pos.count<PAWN>() - 4 * shuffling)
            + optimism * (154 + npm +     pos.count<PAWN>() - 2 * shuffling)) / 1096;
   }
 
