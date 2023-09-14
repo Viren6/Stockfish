2ab1b95a80d947208cec393a6b9a9c0bd41f5a0f
Viren6 <94880762+Viren6@users.noreply.github.com>
2023-09-14 22:12:31 +0100

    Take 1
    bench 1275085

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -175,13 +175,13 @@ Value Eval::evaluate(const Position& pos) {
       optimism += optimism * (nnueComplexity + abs(simpleEval - nnue)) / 512;
       nnue     -= nnue     * (nnueComplexity + abs(simpleEval - nnue)) / 32768;
 
+      optimism = optimism * (100 - shuffling) / 100;
+      nnue = nnue * (200 - shuffling) / 200;
+
       int npm = pos.non_pawn_material() / 64;
       v = (  nnue     * (915 + npm + 9 * pos.count<PAWN>())
            + optimism * (154 + npm +     pos.count<PAWN>())) / 1024;
   }
-
-  // Damp down the evaluation linearly when shuffling
-  v = v * (200 - shuffling) / 214;
 
   // Guarantee evaluation does not hit the tablebase range
   v = std::clamp(v, VALUE_TB_LOSS_IN_MAX_PLY + 1, VALUE_TB_WIN_IN_MAX_PLY - 1);
