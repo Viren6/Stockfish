185b2b8d572f3bc6b17655fc00503ca39ec966f7
Viren6 <94880762+Viren6@users.noreply.github.com>
2023-10-01 13:45:13 +0100

    Take 2
    bench 1367415

--- src/search.cpp
+++ src/search.cpp
@@ -1157,7 +1157,7 @@ moves_loop: // When in check, search starts here
       // Increase reduction on repetition (~1 Elo)
       if (   move == (ss-4)->currentMove
           && pos.has_repeated())
-          r += 2 + 2 * (pos.rule50_count() > 15); 
+          r += 2 + pos.rule50_count() / 15; 
 
       // Increase reduction if next ply has a lot of fail high (~5 Elo)
       if ((ss+1)->cutoffCnt > 3)
