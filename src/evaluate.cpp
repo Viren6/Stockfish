92427ac28156fd4e955b3ec89cdb5ac2104bef8f
Viren6 <94880762+Viren6@users.noreply.github.com>
2023-12-28 08:16:35 +0000

    Take 6
    bench 1354765

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -176,7 +176,7 @@ Value Eval::evaluate(const Position& pos) {
         int   nnueComplexity;
         Value nnue = NNUE::evaluate(pos, true, &nnueComplexity);
 
-        nnueComplexity = (nnueComplexity * (128 * pos.count<ALL_PIECES>())) / 1024;
+        nnueComplexity = (nnueComplexity * (32 * pos.count<ALL_PIECES>())) / 1024;
 
         Value optimism = pos.this_thread()->optimism[stm];
 
