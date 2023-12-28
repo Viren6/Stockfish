22ad13105f68210ac05c10f296fc12f0b9c0bace
Viren6 <94880762+Viren6@users.noreply.github.com>
2023-12-28 12:10:03 +0000

    Take 13
    bench 1432397

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -176,7 +176,7 @@ Value Eval::evaluate(const Position& pos) {
         int   nnueComplexity;
         Value nnue = NNUE::evaluate(pos, true, &nnueComplexity);
 
-        nnueComplexity = nnueComplexity * (PawnValue * pos.count<PAWN>() + pos.non_pawn_material()) / 32768;
+        nnueComplexity = (nnueComplexity * (24 * pos.count<ALL_PIECES>())) / 1024;
 
         Value optimism = pos.this_thread()->optimism[stm];
 
