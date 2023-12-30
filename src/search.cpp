9bd3ed18bf8788bc7e00671562f5b486f1bf573f
Viren6 <94880762+Viren6@users.noreply.github.com>
2023-12-30 10:14:30 +0000

    Take 2
    bench 1422491

--- src/search.cpp
+++ src/search.cpp
@@ -1148,10 +1148,6 @@ moves_loop:  // When in check, search starts here
         if (singularQuietLMR)
             r--;
 
-        // Increase reduction on repetition (~1 Elo)
-        if (move == (ss - 4)->currentMove && pos.has_repeated())
-            r += 2;
-
         // Increase reduction if next ply has a lot of fail high (~5 Elo)
         if ((ss + 1)->cutoffCnt > 3)
             r++;
@@ -1159,7 +1155,7 @@ moves_loop:  // When in check, search starts here
         // Set reduction to 0 for first picked move (ttMove) (~2 Elo)
         // Nullifies all previous reduction adjustments to ttMove and leaves only history to do them
         else if (move == ttMove)
-            r = 0 + cutNode;
+            r = 0 + (cutNode && ttCapture); 
 
         ss->statScore = 2 * thisThread->mainHistory[us][from_to(move)]
                       + (*contHist[0])[movedPiece][to_sq(move)]
