c54163e21cf80ac496f2dedabd8d871ab1d24930
Viren6 <94880762+Viren6@users.noreply.github.com>
2024-01-08 18:25:53 +0000

    Take 7
    bench 1469915

--- src/search.cpp
+++ src/search.cpp
@@ -1243,9 +1243,6 @@ moves_loop:  // When in check, search starts here
         else if (!PvNode || moveCount > 1)
         {
             // Increase reduction if ttMove is not present (~1 Elo)
-            if (!ttMove)
-                r += 2;
- 
             r += (ss+1)->cutoffCnt / 5;
 
             // Note that if expected reduction is high, we reduce search depth by 1 here (~9 Elo)
