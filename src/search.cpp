7336f7c24c85a891ec3ae8800400f16f09f33b54
Viren6 <94880762+Viren6@users.noreply.github.com>
2024-01-13 10:15:20 +0000

    Take 5
    bench 1324960

--- src/search.cpp
+++ src/search.cpp
@@ -1166,7 +1166,7 @@ moves_loop:  // When in check, search starts here
 
         // Increase reduction for cut nodes (~3 Elo)
         if (cutNode)
-            r += 2 + (ss->ttHit && ttValue > beta);
+            r += 2 + (ss->ttHit && ttValue > beta && tte->depth() >= depth);
 
         // Increase reduction if ttMove is a capture (~3 Elo)
         if (ttCapture)
