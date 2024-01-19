136751c448343071b98c8aad59c7666f6aafe26c
Viren6 <94880762+Viren6@users.noreply.github.com>
2024-01-19 06:11:36 +0000

    Take 1
    bench 1305502

--- src/search.cpp
+++ src/search.cpp
@@ -1156,7 +1156,7 @@ moves_loop:  // When in check, search starts here
             if (tte->depth() >= depth)
                 r -= (ttValue > beta) 
                    + cutNode
-                   - (tte->bound() == BOUND_UPPER && PvNode && ttValue < alpha); 
+                   - (ttValue < alpha); 
         }
 
         // Decrease reduction if opponent's move count is high (~1 Elo)
