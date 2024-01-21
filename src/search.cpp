60cd2ab068866472fd0bfaf336f82c52d7089a1f
Viren6 <94880762+Viren6@users.noreply.github.com>
2024-01-21 05:50:19 +0000

    Take 4
    bench 1322280

--- src/search.cpp
+++ src/search.cpp
@@ -1115,9 +1115,9 @@ moves_loop:  // When in check, search starts here
                     extension = -1;
             }
 
-            else if (pos.rule50_count() > 40 && type_of(movedPiece) == PAWN && !capture
+            else if (pos.rule50_count() > 60 && type_of(movedPiece) == PAWN && !capture
                      && move.type_of() != PROMOTION && improving)
-                extension = 1 + (pos.rule50_count() > 60);
+                extension = 2;
 
             // Check extensions (~1 Elo)
             else if (givesCheck && depth > 10)
