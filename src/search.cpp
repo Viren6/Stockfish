e30f53dcd673f59d1b5f9dd1200313a3599edd9c
Viren6 <94880762+Viren6@users.noreply.github.com>
2024-01-23 07:18:48 +0000

    Take 2
    bench 1366938

--- src/search.cpp
+++ src/search.cpp
@@ -1116,7 +1116,7 @@ moves_loop:  // When in check, search starts here
                     extension = -1;
             }
             else if (PvNode && move == ttMove && tte->bound() != BOUND_UPPER && ss->ttPv
-                     && ttValue > alpha && (ss + 1)->cutoffCnt < 4)
+                     && ttValue > beta && tte->depth() >= depth && (ss + 1)->cutoffCnt < 4)
                 extension = 1;
             // Check extensions (~1 Elo)
             else if (givesCheck && depth > 10)
