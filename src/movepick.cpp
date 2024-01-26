35b5bcea52996e2f66559157c6c706b3f98574df
Viren6 <94880762+Viren6@users.noreply.github.com>
2024-01-26 11:59:20 +0000

    Take 1
    bench 1240450

--- src/movepick.cpp
+++ src/movepick.cpp
@@ -205,6 +205,9 @@ void MovePicker::score() {
                           : pt != PAWN ? bool(to & threatenedByPawn) * 15000
                                        : 0)
                        : 0;
+
+            if (pt == PAWN)
+                m.value += pos.rule50_count() * 500 / (depth * depth);
         }
 
         else  // Type == EVASIONS
