83bace58cff0af76473316647187624f2c78e1e9
Viren6 <94880762+Viren6@users.noreply.github.com>
2024-01-30 16:29:45 +0000

    Revert "Experiment 12"
    This reverts commit d48226e98cbb19fc743285c5cf088cfe8bed8b5b.

--- src/search.cpp
+++ src/search.cpp
@@ -1047,13 +1047,8 @@ moves_loop:  // When in check, search starts here
                     // Avoid search explosion by limiting the number of double extensions
                     if (!PvNode && value < singularBeta - 2 && ss->doubleExtensions <= 15)
                     {
-                        extension = 2;
+                        extension = 2 + (value < singularBeta - 80 && !ttCapture);
                         depth += depth < 15;
-                        if (value < singularBeta - 80 && !ttCapture)
-                        { 
-                            extension = 3;
-                            singularQuietLMR = false;
-                        }
                     }
                 }
 
@@ -1138,7 +1133,7 @@ moves_loop:  // When in check, search starts here
             r--;
 
         // Decrease reduction if a quiet ttMove has been singularly extended (~1 Elo)
-        if (singularQuietLMR)
+        if (singularQuietLMR && move != ttMove)
             r--;
 
         // Increase reduction on repetition (~1 Elo)
