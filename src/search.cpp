b28394d2031db64a8bbabdcadbbbc8f9ab6714b7
Viren6 <94880762+Viren6@users.noreply.github.com>
2024-02-04 03:27:50 +0000

    Take 10
    bench 1604128

--- src/search.cpp
+++ src/search.cpp
@@ -1045,7 +1045,7 @@ moves_loop:  // When in check, search starts here
                         if (value < singularBeta - 78 && !ttCapture)
                         { 
                             extension = 3;
-                            depth += ss->ttPv;
+                            depth += (depth < 8 && ss->ttPv);
                         }
                     }
                 }
