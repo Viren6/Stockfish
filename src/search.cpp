93d77ad7e4891adb94e991d64f7e0f9a8d141363
Viren6 <94880762+Viren6@users.noreply.github.com>
2024-02-07 05:28:47 +0000

    Take 3
    bench 1485990

--- src/search.cpp
+++ src/search.cpp
@@ -1039,7 +1039,8 @@ moves_loop:  // When in check, search starts here
                     // Avoid search explosion by limiting the number of double extensions
                     if (!PvNode && ss->doubleExtensions <= 16)
                     {
-                        extension = 2 + (value < singularBeta - 78 && !ttCapture);
+                        extension =
+                          2 + (value < singularBeta - 82 + ((ss + 1)->cutoffCnt > 3) * 50 && !ttCapture);
                         depth += depth < 16;
                     }
                 }
