d512f50dbb1c3b64226785ce6f8e76fcb643499c
Viren6 <94880762+Viren6@users.noreply.github.com>
2024-02-10 00:19:59 +0000

    Take 2
    bench 1266888

--- src/search.cpp
+++ src/search.cpp
@@ -1019,8 +1019,8 @@ moves_loop:  // When in check, search starts here
             // scaling. Their values are optimized to time controls of 180+1.8 and longer
             // so changing them requires tests at these types of time controls.
             // Recursive singular search is avoided.
-            if (!rootNode && move == ttMove && !excludedMove
-                && depth >= 3 + ttCapture + ss->ttPv 
+            if (!rootNode && move == ttMove && !excludedMove 
+                && depth >= 3 - (thisThread->completedDepth > 29) + ttCapture + ss->ttPv 
                 && std::abs(ttValue) < VALUE_TB_WIN_IN_MAX_PLY && (tte->bound() & BOUND_LOWER)
                 && tte->depth() >= depth - 3)
             {
