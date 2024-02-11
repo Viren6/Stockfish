818668e67ef806da888bb6a37c76f54521dea424
Viren6 <94880762+Viren6@users.noreply.github.com>
2024-02-11 00:30:32 +0000

    Revert "Take 8"
    This reverts commit 7624bfc24beca682c835a72640d66d20fdad0737.

--- src/search.cpp
+++ src/search.cpp
@@ -60,7 +60,7 @@ Value futility_margin(Depth d, bool noTtCutNode, bool improving) {
 }
 
 constexpr int futility_move_count(bool improving, Depth depth, bool noTtCutNode) {
-    return (40 + 10 * depth * depth / (1 + noTtCutNode)) / (20 - 10 * improving);
+    return (4 + depth * depth / (1 + noTtCutNode)) / (2 - improving);
 }
 
 // Add correctionHistory value to raw staticEval and guarantee evaluation does not hit the tablebase range
