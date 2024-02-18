f24abc244221d8b74e6e110e3de86b12b9845489
Viren6 <94880762+Viren6@users.noreply.github.com>
2024-02-18 07:32:51 +0000

    Take 4
    bench 1283578

--- src/search.cpp
+++ src/search.cpp
@@ -1045,7 +1045,7 @@ moves_loop:  // When in check, search starts here
                         if (value < singularBeta - 78 && !ttCapture)
                         { 
                             extension = 3;
-                            singularQuietLMR = true;
+                            singularQuietLMR = (value < singularBeta - 200);
                         }
                     }
                 }
