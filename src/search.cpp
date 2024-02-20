f023df0b758dae275801540574375d7f39b48a49
Viren6 <94880762+Viren6@users.noreply.github.com>
2024-02-20 21:48:30 +0000

    Take 10
    bench 1374277

--- src/search.cpp
+++ src/search.cpp
@@ -1117,14 +1117,14 @@ moves_loop:  // When in check, search starts here
         // Set reduction to 0 for first picked move (ttMove) (~2 Elo)
         // Nullifies all previous reduction adjustments to ttMove and leaves only history to do them
         else if (move == ttMove)
-            r = 1;
+            r = 0;
 
         // Decrease reduction if position is or has been on the PV (~7 Elo)
         if (ss->ttPv)
             r -= 1 + PvNode + (ttValue > alpha) + (tte->depth() >= depth) * (1 + cutNode);
 
-        if (move == ttMove && (ss + 1)->cutoffCnt < 4)
-            r *= 2;
+        if (move == ttMove)
+            r = r * 2 + ((ss + 1)->cutoffCnt < 4);
 
         ss->statScore = 2 * thisThread->mainHistory[us][move.from_to()]
                       + (*contHist[0])[movedPiece][move.to_sq()]
