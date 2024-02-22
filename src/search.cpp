fb60dcaf23a28131946e90b98aab063ad3f4cb97
Viren6 <94880762+Viren6@users.noreply.github.com>
2024-02-22 23:39:14 +0000

    Take 16
    bench 1347723

--- src/search.cpp
+++ src/search.cpp
@@ -1125,7 +1125,7 @@ moves_loop:  // When in check, search starts here
         // Set reduction to 0 for first picked move (ttMove) (~2 Elo)
         // Nullifies all previous reduction adjustments to ttMove and leaves only history to do them
         else if (move == ttMove)
-            r = 1 - (ss->ttPv && ttValue > alpha);
+            r = 1;
 
         if (ss->ttPv && tte->depth() >= depth && cutNode)
             r--;
