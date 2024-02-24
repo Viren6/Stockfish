c660610d7e578c9db97f3a18514c560e615bebd8
Viren6 <94880762+Viren6@users.noreply.github.com>
2024-02-24 18:26:56 +0000

    Take 1
    bench 1761078

--- src/search.cpp
+++ src/search.cpp
@@ -1040,7 +1040,12 @@ moves_loop:  // When in check, search starts here
                     // We make sure to limit the extensions in some way to avoid a search explosion
                     if (!PvNode && ss->multipleExtensions <= 16)
                     {
-                        extension = 2 + (value < singularBeta - 78 && !ttCapture);
+                        extension = 2;
+                        if (value < singularBeta - 82 + ((ss + 1)->cutoffCnt > 3) * 50
+                            && !ttCapture)
+                        {
+                            extension = 3 + (ttValue > alpha && tte->depth() >= depth);
+                        }
                         depth += depth < 16;
                     }
                 }
