764821a62e58792fc8418a8115e39f41adafa914
Viren6 <94880762+Viren6@users.noreply.github.com>
2024-02-24 23:51:52 +0000

    Take 1
    bench 1459395

--- src/search.cpp
+++ src/search.cpp
@@ -1176,6 +1176,9 @@ moves_loop:  // When in check, search starts here
             if (!ttMove)
                 r += 2;
 
+            else if ((ss + 1)->cutoffCnt > 3)
+                r += 2;
+
             // Note that if expected reduction is high, we reduce search depth by 1 here (~9 Elo)
             value = -search<NonPV>(pos, ss + 1, -(alpha + 1), -alpha, newDepth - (r > 3), !cutNode);
         }
