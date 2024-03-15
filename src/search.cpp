372139873a8924d9ef77b0eda46522d27e272940
Viren6 <94880762+Viren6@users.noreply.github.com>
2024-03-15 05:47:29 +0000

    Take 6
    bench 2269955

--- src/search.cpp
+++ src/search.cpp
@@ -1025,7 +1025,7 @@ moves_loop:  // When in check, search starts here
                 && tte->depth() >= depth - 3)
             {
                 Value singularBeta = ttValue - (58 + 55 * (ss->ttPv && !PvNode)) * depth / 64
-                                   + (tte->depth() - depth) * 6;
+                                   + (tte->depth() - depth) * 2;
                 Depth singularDepth = newDepth / 2;
 
                 ss->excludedMove = move;
