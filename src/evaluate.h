1efebd21a0fc4c5271e66f7a07e9156661bdfaca
Viren6 <94880762+Viren6@users.noreply.github.com>
2024-03-18 03:47:38 +0000

    110k games
    bench 2154941

--- src/evaluate.h
+++ src/evaluate.h
@@ -35,7 +35,7 @@ constexpr inline int SmallNetThreshold = 1136, PsqtOnlyThreshold = 2656;
 // for the build process (profile-build and fishtest) to work. Do not change the
 // name of the macro or the location where this macro is defined, as it is used
 // in the Makefile/Fishtest.
-#define EvalFileDefaultNameBig "nn-fea897cc89d3.nnue"
+#define EvalFileDefaultNameBig "nn-a5edbe54458a.nnue"
 #define EvalFileDefaultNameSmall "nn-baff1ede1f90.nnue"
 
 namespace NNUE {
