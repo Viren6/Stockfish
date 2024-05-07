29bca417a3dfc2301ed6c5a28d711cc7b96afbdc
Viren6 <94880762+Viren6@users.noreply.github.com>
2024-05-08 00:34:20 +0100

    Revert "Take 3"
    This reverts commit b6e883f203d0cc7532391abde60087a2730f044f.

--- src/search.cpp
+++ src/search.cpp
@@ -1037,7 +1037,7 @@ moves_loop:  // When in check, search starts here
             // so changing them requires tests at these types of time controls.
             // Recursive singular search is avoided.
             if (!rootNode && move == ttMove && !excludedMove
-                && depth >= 4 - (thisThread->completedDepth > 32) + ss->ttPv
+                && depth >= 5
                 && std::abs(ttValue) < VALUE_TB_WIN_IN_MAX_PLY && (tte->bound() & BOUND_LOWER)
                 && tte->depth() >= depth - 3)
             {
@@ -1098,9 +1098,9 @@ moves_loop:  // When in check, search starts here
             else if (!rootNode && move == ttMove && !excludedMove
                      && std::abs(ttValue) < VALUE_TB_WIN_IN_MAX_PLY && (tte->bound() & BOUND_LOWER)
                      && tte->depth() > 0 && !ss->ttPv && !ttCapture && depth >= 3
-                     && tte->depth() >= depth - 4)
+                     && tte->depth() >= depth - 3)
             { 
-                Value singularBeta  = ttValue - 10 - depth;
+                Value singularBeta  = ttValue - depth;
                 Depth singularDepth = newDepth / 2;
 
                 ss->excludedMove = move;
@@ -1109,9 +1109,11 @@ moves_loop:  // When in check, search starts here
                 ss->excludedMove = Move::none();
 
                 if (value < singularBeta)
-                    extension = 1;
+                    extension = 1 + (value < singularBeta - 150);
                 else if (singularBeta >= beta)
-                    extension = -1;
+                    extension = -2;
+
+                depth += (extension == 2);
 
             }
 
