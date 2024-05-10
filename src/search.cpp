e44fc35dde09965e5fb09e8ca0fc69e576e75534
Viren6 <94880762+Viren6@users.noreply.github.com>
2024-05-10 06:25:05 +0100

    Take 1
    bench 908107

--- src/search.cpp
+++ src/search.cpp
@@ -1049,14 +1049,7 @@ moves_loop:  // When in check, search starts here
 
                 if (value < singularBeta)
                 {
-                    int doubleMargin = 251 * PvNode - 241 * !ttCapture;
-                    int tripleMargin =
-                      135 + 234 * PvNode - 248 * !ttCapture + 124 * (ss->ttPv || !ttCapture);
-                    int quadMargin = 447 + 354 * PvNode - 300 * !ttCapture + 206 * ss->ttPv;
-
-                    extension = 1 + (value < singularBeta - doubleMargin)
-                              + (value < singularBeta - tripleMargin)
-                              + (value < singularBeta - quadMargin);
+                    extension = 1;
 
                     depth += ((!PvNode) && (depth < 14));
                 }
