c2b43213b9aa430175e7a5786e575a8e84dfe0fd
Viren6 <94880762+Viren6@users.noreply.github.com>
2024-05-14 01:19:15 +0100

    Take 5
    bench 1876282

--- src/search.cpp
+++ src/search.cpp
@@ -452,7 +452,7 @@ void Search::Worker::iterative_deepening() {
             double totalTime = mainThread->tm.optimum() * fallingEval * reduction
                              * bestMoveInstability * EvalLevel[el] * recapture;
 
-            totalTime = (totalTime * 3.0 + opponentUsedTime * 0.8) / 4.0;
+            totalTime = (totalTime * 3.0 + opponentUsedTime) / 4.0;
 
             // Cap used time in case of a single legal move for a better viewer experience
             if (rootMoves.size() == 1)
