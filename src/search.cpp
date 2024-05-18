d99d8bcc9781d0c7a8b340c62664dd283e253d04
Viren6 <94880762+Viren6@users.noreply.github.com>
2024-05-18 08:42:46 +0100

    Take 1
    bench 2567524

--- src/search.cpp
+++ src/search.cpp
@@ -1057,6 +1057,10 @@ moves_loop:  // When in check, search starts here
                     extension = 1 + (value < singularBeta - doubleMargin)
                               + (value < singularBeta - tripleMargin);
 
+                    if ((value < singularBeta - tripleMargin - 50) && (type_of(movedPiece) == PAWN))
+                        extension = 4;
+
+
                     depth += ((!PvNode) && (depth < 14));
                 }
 
