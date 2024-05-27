63ce61d7b594c54990755162df2d83bfeb3b9496
Viren6 <94880762+Viren6@users.noreply.github.com>
2024-05-27 19:35:49 +0100

    Take 6
    bench 1716333

--- src/search.cpp
+++ src/search.cpp
@@ -1065,7 +1065,7 @@ moves_loop:  // When in check, search starts here
                     int doubleMargin = 304 * PvNode - 203 * !ttCapture;
                     int tripleMargin = 117 + 259 * PvNode - 296 * !ttCapture + 97 * ss->ttPv;
                     int quadMargin   = 516 + 343 * PvNode - 273 * !ttCapture + 232 * ss->ttPv
-                                       + 100 * (ss-1)->ttPv - 200 * !priorCapture;
+                                       + 100 * (ss-1)->ttPv - 200 * !priorCapture; 
 
                     extension = 1 + (value < singularBeta - doubleMargin)
                               + (value < singularBeta - tripleMargin)
