702f02d7ecbbcdd3422ff4e30e9acdc044e11c4a
Viren6 <94880762+Viren6@users.noreply.github.com>
2024-05-29 19:28:09 +0100

    Take 2
    bench 1379168

--- src/search.cpp
+++ src/search.cpp
@@ -1150,16 +1150,13 @@ moves_loop:  // When in check, search starts here
             r++;
 
         // Increase reduction if next ply has a lot of fail high (~5 Elo)
-        if ((ss + 1)->cutoffCnt > 3)
+        if ((ss + 1)->cutoffCnt > 3 || (ss + 1)->extensions > 3)
             r++;
 
         // Set reduction to 0 for first picked move (ttMove) (~2 Elo)
         // Nullifies all previous reduction adjustments to ttMove and leaves only history to do them
         else if (move == ttMove)
             r = 0;
-
-        if((ss + 1)->extensions > 3)
-            r++;
 
         ss->statScore = 2 * thisThread->mainHistory[us][move.from_to()]
                       + (*contHist[0])[movedPiece][move.to_sq()]
