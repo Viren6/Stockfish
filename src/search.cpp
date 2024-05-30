812a916dd1c24f9758cccb02275fa9d49145ec8f
Viren6 <94880762+Viren6@users.noreply.github.com>
2024-05-30 16:51:03 +0100

    Take 1: P[16], Less
    bench 1272798

--- src/search.cpp
+++ src/search.cpp
@@ -1289,7 +1289,7 @@ moves_loop:  // When in check, search starts here
 
                 if (value >= beta)
                 {
-                    ss->cutoffCnt += 1 + !ttMove - (extension >= 2);
+                    ss->cutoffCnt += 1 + !ttMove - (move == ttMove);
                     assert(value >= beta);  // Fail high
                     break;
                 }
