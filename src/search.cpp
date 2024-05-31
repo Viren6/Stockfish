d66b247a6029b3be03448a82a092a82d629c4bb5
Viren6 <94880762+Viren6@users.noreply.github.com>
2024-05-31 13:07:07 +0100

    Take 11: P[29], Less
    bench 1187500

--- src/search.cpp
+++ src/search.cpp
@@ -1289,8 +1289,7 @@ moves_loop:  // When in check, search starts here
 
                 if (value >= beta)
                 {
-                    ss->cutoffCnt += 1 + !ttMove - (extension >= 2)
-                                   + (type_of(movedPiece) == KNIGHT) - (move == ttMove); 
+                    ss->cutoffCnt += 1 + !ttMove - (extension >= 2) - (!(ss - 1)->ttHit); 
                     assert(value >= beta);  // Fail high
                     break;
                 }
