15c1a0146d1a3060ddff6c783174723ad5223e5c
Viren6 <94880762+Viren6@users.noreply.github.com>
2024-05-31 01:18:15 +0100

    Take 10: Combine 2 conditions
    bench 1323617

--- src/search.cpp
+++ src/search.cpp
@@ -1290,7 +1290,7 @@ moves_loop:  // When in check, search starts here
                 if (value >= beta)
                 {
                     ss->cutoffCnt += 1 + !ttMove - (extension >= 2)
-                                   + (type_of(movedPiece) == KNIGHT) - (move == ttMove);
+                                   + (type_of(movedPiece) == KNIGHT) - (move == ttMove); 
                     assert(value >= beta);  // Fail high
                     break;
                 }
