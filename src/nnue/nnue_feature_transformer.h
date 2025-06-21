9d7b59386e8957566633625b19be4586ae569be2
Viren6 <94880762+Viren6@users.noreply.github.com>
2025-06-21 01:45:36 +0100

    Take 4
    bench 2307268

--- src/nnue/nnue_feature_transformer.h
+++ src/nnue/nnue_feature_transformer.h
@@ -201,8 +201,8 @@ class FeatureTransformer {
             static_assert((HalfDimensions / 2) % OutputChunkSize == 0);
             constexpr IndexType NumOutputChunks = HalfDimensions / 2 / OutputChunkSize;
 
-            const vec_t Zero = vec_zero();
-            const vec_t One  = vec_set_16(127 * 2);
+            static const vec_t Zero = vec_zero();
+            static const vec_t One  = vec_set_16(127 * 2);
 
             const vec_t* in0 = reinterpret_cast<const vec_t*>(&(accumulation[perspectives[p]][0]));
             const vec_t* in1 =
